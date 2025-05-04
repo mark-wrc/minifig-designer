d3edf1d60c48f9b28e35a74deb3ff04e550762d8
kowalskiX12 <karlmichaelcrisostomo123@gmail.com>
2025-05-04 20:07:03 +0800

    feat: setup `Module Fedaration`

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
+// source knowledge:
+// https://www.youtube.com/watch?v=t-nchkL9yIg
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: 'minifig_builder',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './BuilderButton': './src/components/BuilderButton',
+        './BuilderApp': './src/App',
+      },
+    }),
+  ],
+
+  server: {
+    port: 3001, //placeholder:Port for the main app
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
