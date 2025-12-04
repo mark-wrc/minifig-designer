6bfb5eda315480c503111cf05f2ff47c28def09b
Animesh Jha <markstorey.wrc@gmail.com>
2025-12-04 15:14:59 +0100

    updated the port and filter

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 const Config = {
-  apiUrl: import.meta.env.VITE_PUBLIC_API_URL || 'http://localhost:4001',
+  apiUrl: import.meta.env.VITE_PUBLIC_API_URL || 'http://localhost:4000',
   checkoutUrl: import.meta.env.VITE_CHECKOUT_URL || 'http://localhost:5173/checkout',
   websiteUrl: import.meta.env.VITE_WEBSITE_URL || 'http://localhost:5173',
   isDevelopment: import.meta.env.MODE === 'development',
